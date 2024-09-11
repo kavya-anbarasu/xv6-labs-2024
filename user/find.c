29b9415d29913284823d340f845b4c5bf91d1bbd
kavya-anbarasu <kavya.anbarasu@gmail.com>
2024-09-10 23:55:56 -0400

    find

---
+++ user/find.c
@@ -0,0 +1,87 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
+
+char* fmtname(char *path)
+{
+  static char buf[DIRSIZ+1];
+  char *p;
+
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  buf[strlen(p)] = 0;
+  return buf;
+}
+
+void find(char *path, char *target) {
+    char buf[512], *p;
+    int fd;
+    struct dirent de;
+    struct stat st;
+
+    if ((fd = open(path, O_RDONLY)) < 0) {
+        fprintf(2, "find: cannot open %s\n", path);
+        return;
+    }
+
+    if (fstat(fd, &st) < 0) {
+        fprintf(2, "find: cannot stat %s\n", path);
+        close(fd);
+        return;
+    }
+
+    // printf("Path is: %s\n", path);
+    // printf("Checking type: %d\n", st.type);
+    switch (st.type) {
+    case T_DEVICE:
+    case T_FILE:
+        // printf("This is a file\n");
+        // printf("fmtname(path): %s and target: %s\n", fmtname(path), target);
+        // int result = strcmp(fmtname(path), target);
+        // printf("Result: %d\n", result); 
+        if (strcmp(fmtname(path), target) == 0) {
+            printf("%s\n", path);
+        }
+        break;
+
+    case T_DIR:
+        // printf("This is a directory\n");
+        if (strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf)) {
+            printf("find: path too long\n");
+            break;
+        }
+        strcpy(buf, path);
+        p = buf + strlen(buf);
+        *p++ = '/';
+        while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+            if (de.inum == 0)
+                continue;
+            memmove(p, de.name, DIRSIZ);
+            p[DIRSIZ] = 0;
+            if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+                continue;
+            // printf("Checking buf: %s\n", buf);
+            find(buf, target);
+        }
+        break;
+    }
+    close(fd);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(2, "Usage: find <directory> <filename>\n");
+        exit(1);
+    }
+    find(argv[1], argv[2]);
+    exit(0);
+}

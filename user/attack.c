9de8316a3384ba89bf1d66332216846fa677ac8f
kavya-anbarasu <kavya.anbarasu@gmail.com>
2024-09-19 23:50:08 -0400

    attack and finishing lab

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,11 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
+  char *mem = sbrk(PGSIZE * 32);
+
+  char *secret = mem + 8 * PGSIZE + 16;
+  printf("secret: %s\n", secret);
+  write(2, secret, 8);
 
   exit(1);
 }
